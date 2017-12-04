4f78e2054244aeb80838376bd7b1369d2f20cf43
Martin Federico Capparelli <mcapparelli@lab72.it.itba.edu.ar>
2017-12-04 16:33:42 -0300

    Printing notes:

---
+++ lalala.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+int main( int argc, const char* argv[] )
+{
+	print_mi();
+		printf("*******\n");
+	print_fa();
+		printf("*******\n");
+	print_sol();
+		printf("*******\n");
+	print_la();
+		printf("*******\n");
+	print_si();
+		printf("*******\n");
+
+}
+
+void print_do(void){
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("-Ø-\n");
+}
+
+
+void print_re(void){
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf(" o \n");
+}
+
+void print_mi(void){
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("-Ø-\n");
+}
+
+void print_fa(void){
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("_o_\n");
+}
+
+void print_sol(void){
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("-Ø-\n");
+	printf("___\n");
+}
+
+void print_la(void){
+	printf("___\n");
+	printf("___\n");
+	printf("___\n");
+	printf("_o_\n");
+	printf("___\n");
+}
+
+void print_si(void){
+	printf("___\n");
+	printf("___\n");
+	printf("-Ø-\n");
+	printf("___\n");
+	printf("___\n");
+}
